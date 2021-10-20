4083c2508fdca8e10ca577544fc4af7a12f90393
Mehrdad Amini <pcmehrdad@gmail.com>
2021-10-20 10:28:09 +0330

    Update lemp-basic.sh

--- lemp-basic.sh
+++ lemp-basic.sh
@@ -6,10 +6,6 @@ if [[ $EUID = 0 ]]; then
 	exit 1
 fi
 
-function get_latest_github_release_number() {
-	curl --silent "https://api.github.com/repos/$1/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
-}
-
 # Set SSH Port
 read -e -p $'Set/Change SSH port : ' -i "22" ssh_port_number
 sudo sed -i 's/#Port 22/Port '$ssh_port_number'/g' /etc/ssh/sshd_config
@@ -19,67 +15,98 @@ sudo service ssh restart
 read -e -p $'Change System TimeZone ? : ' -i "Asia/Tehran" system_default_timezone
 sudo timedatectl set-timezone $system_default_timezone
 
-read -e -p $'www folder name ? : \n' -i "www" www_folder_name
-read -e -p $'Enter domains (seperated by space , exp: test.com www.test.com ) : \n' -i "www" www_domains
 
+read -e -p $'Folder name for domain(s) ? : \n' -i "test.com" domain_folder_name
+read -e -p $'Enter domains FQDN (seperated by space , exp: test.com www.test.com ) : \n' www_domains
+read -e -p $'Select PHP Version [7.4|8.0]: ' -i "8.0" php_version
+read -e -p $'Install Composer [y/n]: ' -i "y" if_install_composer
+read -e -p $'DataBase is MySQL8 / Change it to MariaDb ? : ' -i "y" if_change_db_to_mariadb
+read -e -p $'Enter DataBase root password: \n' database_root_password
 
+read -e -p $'Install PHPMyAdmin on pma folder ? : ' -i "y" if_install_pma
+if [[ $if_install_pma =~ ^([Yy])$ ]]
+then
+	read -e -p $'Enter PHPMyAdmin FQDN: \n' pma_fqdn
+fi
 
-# sudo echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+read -e -p $'Install Redis ? : ' -i "y" if_install_redis
+#############################################################################
+
 sudo apt --yes install software-properties-common aria2 bzip2 ca-certificates curl git gnupg gosu htop iotop iperf libcap2-bin libpng-dev make gcc nano net-tools nmap chrony openssh-server openssl p7zip poppler-utils apt-transport-https lsb-release python2 sqlite3 supervisor traceroute unar unzip wget zip zsh
 
-# nmtui
-# sudo apt --yes install network-manager
-# sudo curl -L "https://github.com/ariadata/ubuntu-lemp/raw/main/files/NetworkManager.conf" -o /etc/NetworkManager/NetworkManager.conf
-# sudo systemctl enable --now network-manager
-
-
-# php
+cd ~
+mkdir -p www
+cd /home/$USER/www/
+mkdir -p $domain_folder_name
 sudo add-apt-repository --yes ppa:ondrej/php
 sudo apt --yes update
-sudo apt --yes install php8.0-cli php8.0-dev php8.0-pgsql php8.0-sqlite3 php8.0-gd php8.0-curl php8.0-memcached php8.0-imap php8.0-mysql php8.0-mbstring php8.0-xml php8.0-zip php8.0-bcmath php8.0-soap php8.0-intl php8.0-readline php8.0-pcov php8.0-msgpack php8.0-igbinary php8.0-ldap php8.0-redis php8.0-swoole php8.0-xdebug
-sudo php -r "readfile('http://getcomposer.org/installer');" | sudo php -- --install-dir=/usr/bin/ --filename=composer
+
+# install php
+if [[ $php_version = "8.0" ]]
+then
+	# install with 8.0
+	sudo apt --yes install php8.0-cli php8.0-dev php8.0-pgsql php8.0-sqlite3 php8.0-gd php8.0-curl php8.0-memcached php8.0-imap php8.0-mysql php8.0-mbstring php8.0-xml php8.0-zip php8.0-bcmath php8.0-soap php8.0-intl php8.0-readline php8.0-pcov php8.0-msgpack php8.0-igbinary php8.0-ldap php8.0-redis php8.0-swoole php8.0-xdebug
+else
+	# install with 7.4
+	sudo apt --yes install php7.4-cli php7.4-dev php7.4-pgsql php7.4-sqlite3 php7.4-gd php7.4-curl php7.4-memcached php7.4-imap php7.4-mysql php7.4-mbstring php7.4-xml php7.4-zip php7.4-bcmath php7.4-soap php7.4-intl php7.4-readline php7.4-pcov php7.4-msgpack php7.4-igbinary php7.4-ldap php7.4-redis php7.4-swoole php7.4-xdebug
+fi
+
+# install composer
+if [[ $if_install_composer =~ ^([Yy])$ ]]
+then
+	sudo php -r "readfile('http://getcomposer.org/installer');" | sudo php -- --install-dir=/usr/bin/ --filename=composer
+fi
+
+# install pma
+if [[ $if_install_pma =~ ^([Yy])$ ]]
+then
+	mkdir -p pma
+	curl -L "https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-english.tar.gz" -o pma.tgz
+	tar -xzf pma.tgz -C pma --strip-components=1
+	# copy config
+fi
+
+# install redis
+if [[ $if_install_redis =~ ^([Yy])$ ]]
+then
+	# install redis
+fi
+
+# Install MySQL Server based on selection
+if [[ $if_change_db_to_mariadb =~ ^([Yy])$ ]]
+then
+	# install mariadb
+	sudo apt --yes install 
+else
+	# install mysql8
+	sudo apt --yes install 
+fi
+
+## change mysql-mariadb root password
+mysql_secure_installation <<EOF
+
+y
+y
+$database_root_password
+$database_root_password
+y
+y
+y
+y
+EOF
+
+# install nginx + configuration (nginx+phpfpm)
+
+
+
 sudo apt --yes update && sudo apt -q --yes upgrade
 sudo apt --yes autoremove
+clear
 
-# docker + docker-compose
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt --yes update
-sudo apt --yes install docker-ce docker-ce-cli containerd.io
-docker_compose_latest_version="$(get_latest_github_release_number docker/compose)"
-sudo curl -L "https://github.com/docker/compose/releases/download/$docker_compose_latest_version/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
-sudo chmod +x /usr/bin/docker-compose
-sudo usermod -aG docker $USER
-sudo chmod 666 /var/run/docker.sock
-sudo systemctl enable --now docker
-
-sudo apt --yes update && sudo apt -q --yes upgrade
-
-# laravel + larave-new command in bash
-composer global require laravel/installer --update-with-all-dependencies
-echo 'export PATH="$HOME/.config/composer/vendor/bin:$PATH"' >> ~/.bashrc
-# source ~/.bashrc
-# sudo chown -R $USER:$USER /home/$USER/.config/composer
-### sail install alias
-echo "alias sail='./vendor/bin/sail'" >> ~/.bash_aliases
-echo 'laravel-new(){
-	curl -s "https://laravel.build/$1" | bash
-	sudo sed -i 's/laravel.test/'"$1"'.test/g' $1/docker-compose.yml
-}' >> ~/.bash_aliases
-source ~/.bash_aliases
-
-# install portainer
-docker pull portainer/portainer-ce:latest
-docker run -d -p 9999:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
-
-# install phpmyadmin
-# docker run --name phpmyadmin --restart=always -d -p 9998:80 -e PMA_HOST=host.docker.internal -e MYSQL_USERNAME=sail -e MYSQL_ROOT_PASSWORD=password phpmyadmin
-
-# install nginx-proxy-manager
-# Login : http://IP_ADDR:81
-# User  : admin@example.com
-# Pass  : changeme
-docker run -d -p 80:80 -p 443:443 -p 81:81 --name=nginx-proxy-manager --restart=unless-stopped -e DB_SQLITE_FILE=/data/database.sqlite -e DISABLE_IPV6=true -v /data:/data -v /letsencrypt:/etc/letsencrypt jc21/nginx-proxy-manager
-
-sudo apt --yes update && sudo apt -q --yes upgrade
-# sudo reboot
+## reboot at the end
+read -e -p $'Finished, Reboot Now ? : ' -i "y" if_reboot_at_end
+if [[ $if_reboot_at_end =~ ^([Yy])$ ]]
+then
+	sudo reboot
+	exit
+fi
