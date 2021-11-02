390f86f4837548c5306bf056e7afe9847208e477
Mehrdad Amini <pcmehrdad@gmail.com>
2021-11-02 21:20:48 +0330

    xdebug line fixed

--- lemp-basic.sh
+++ lemp-basic.sh
@@ -55,14 +55,14 @@ then
 	fi
 	if [[ $if_install_php_imagick =~ ^([Yy])$ ]]
 	then
-		sudo apt --yes install php8.0-xdebug
+		sudo apt --yes install php8.0-imagick
 	fi
 else
 	# install with 7.4
 	sudo apt --yes install php7.4-cli php7.4-fpm php7.4-dev php7.4-pgsql php7.4-sqlite3 php7.4-gd php7.4-curl php7.4-memcached php7.4-imap php7.4-mysql php7.4-mbstring php7.4-xml php7.4-zip php7.4-bcmath php7.4-soap php7.4-intl php7.4-readline php7.4-pcov php7.4-msgpack php7.4-igbinary php7.4-ldap php7.4-redis php7.4-swoole php7.4-apcu php7.4-opcache
 	if [[ $if_install_php_xdebug =~ ^([Yy])$ ]]
 	then
-		sudo apt --yes install php8.0-imagick
+		sudo apt --yes install php7.4-xdebug
 	fi
 	if [[ $if_install_php_imagick =~ ^([Yy])$ ]]
 	then
