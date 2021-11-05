8f62b9997e2e067251156a3ad343b3139c0e8842
Mehrdad Amini <pcmehrdad@gmail.com>
2021-11-05 22:43:11 +0330

    added bash_aliases

--- lemp-basic.sh
+++ lemp-basic.sh
@@ -168,6 +168,37 @@ sudo curl -L "https://github.com/ariadata/ubuntu-sh/raw/master/files/mysql-custo
 ## create user mariadb-mysql
 # sudo mysql -u root -p${database_root_password} -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$database_root_password' WITH GRANT OPTION;FLUSH PRIVILEGES;"
 
+#### custom bash_aliases
+echo 'extract () {
+	for archive in $*; do
+		if [ -f $archive ] ; then
+			case $archive in
+				*.tar.bz2)   tar xvjf $archive    ;;
+				*.tar.gz)    tar xvzf $archive    ;;
+				*.bz2)       bunzip2 $archive     ;;
+				*.rar)       unar $archive       ;;
+				*.gz)        gunzip $archive      ;;
+				*.tar)       tar xvf $archive     ;;
+				*.tbz2)      tar xvjf $archive    ;;
+				*.tgz)       tar xvzf $archive    ;;
+				*.zip)       unzip $archive       ;;
+				*.Z)         uncompress $archive  ;;
+				*.7z)        7za x $archive        ;;
+				*)           echo "do not know how to extract $archive..." ;;
+			esac
+		else
+			echo "$archive is not a valid file!"
+		fi
+	done
+}
+alias sc="systemctl"
+alias myip4="whatsmyip4"
+function whatsmyip4 (){
+	curl -s "https://myip4.ir/";
+}
+' >> ~/.bash_aliases
+## end bash_aliases
+
 ## 
 sudo apt --yes update && sudo apt -q --yes upgrade
 sudo apt --yes autoremove
