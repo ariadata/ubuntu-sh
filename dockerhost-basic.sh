8d16293cdeb407622bbc7b34fa233b21fef0d65f
Mehrdad Amini <pcmehrdad@gmail.com>
2022-03-18 02:16:35 +0330

    Update dockerhost-basic.sh

--- dockerhost-basic.sh
+++ dockerhost-basic.sh
@@ -24,7 +24,7 @@ then
 	sudo apt --yes update && sudo apt -q --yes upgrade
 fi
 # sudo echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-sudo apt --yes install wget curl git nano lsb-release sqlite3 p7zip apt-transport-https ca-certificates software-properties-common cron
+sudo apt --yes install wget curl git nano lsb-release sqlite3 p7zip gnupg-agent apt-transport-https ca-certificates software-properties-common cron
 sudo systemctl enable --now cron
 
 ## docker + docker-compose
