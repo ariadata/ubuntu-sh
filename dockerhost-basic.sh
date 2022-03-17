85eba849a29c1de0308c8ba10387e1628613a3a8
Mehrdad Amini <pcmehrdad@gmail.com>
2022-03-18 02:19:10 +0330

    Update dockerhost-basic.sh

--- dockerhost-basic.sh
+++ dockerhost-basic.sh
@@ -31,6 +31,7 @@ sudo systemctl enable --now cron
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt --yes update
+sudo apt-cache policy docker-ce
 sudo apt --yes install docker-ce docker-ce-cli containerd.io
 
 docker_compose_latest_version="$(get_latest_github_release_number docker/compose)"
